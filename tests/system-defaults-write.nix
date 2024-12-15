d71a864d2c970b4b73893b062898cba1f7f80b49
Frank Chiarulli Jr <frank@frankchiarulli.com>
2024-12-15 01:26:25 -0500

    allow modifying display type of persistent-others

--- tests/system-defaults-write.nix
+++ tests/system-defaults-write.nix
@@ -80,7 +80,16 @@
     { folder = "/Applications/Utilities"; }
     { file = "/Users/example/Downloads/test.csv"; }
   ];
-  system.defaults.dock.persistent-others = ["~/Documents" "~/Downloads/file.txt"];
+  system.defaults.dock.persistent-others = [
+    "~/Documents"
+    "~/Downloads/file.txt"
+    {
+      path = "~/Appications";
+      displayas = "folder";
+      showas = "fan";
+      arrangement = "date-modified";
+    }
+  ];
   system.defaults.dock.scroll-to-open = false;
   system.defaults.finder.AppleShowAllFiles = true;
   system.defaults.finder.ShowStatusBar = true;
